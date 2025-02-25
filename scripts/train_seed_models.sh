949530c27a137330c03c463997baac1327c55ecd
oguzhanersoy <oguzer2006@gmail.com>
2025-02-25 17:40:06 +0100

    fixing typo

--- scripts/train_seed_models.sh
+++ scripts/train_seed_models.sh
@@ -6,7 +6,7 @@ if [ ! -d "$ROOT/hdee/experiments/" ]; then
     mkdir $ROOT/hdee/experiments/;
 fi;
 
-mkdir $ROOT/hdee/experiments/hdee_seed_models
+mkdir $ROOT/hdee/experiments/seed_models
 
 for config in "train_seed_expert_s.yaml" "train_seed_expert_m.yaml" "train_seed_expert_l.yaml"; do
  
